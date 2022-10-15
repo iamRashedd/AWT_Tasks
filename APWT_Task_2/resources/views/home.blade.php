e0561df93738fb67569f5a5866607ec542d6ffb3
Rashedul Islam <rislam727433@gmail.com>
2022-10-15 20:30:37 +0600

    Previous task

---
+++ APWT_Task_2/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <h1>Homepage</h1>
+        <h4>welcome to the homepage</h4>
+    </div>
+@endsection
---
+++ APWT_Task_2/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/hello', function () {
+    return view('hello');
+});
+
+Route::get('/index',[PagesController::class,'index'])->name("home");
+Route::get('/profile',[PagesController::class,'profile'])->name("profile");
+
+
+Route::get('/',[ProductController::class,'index'])->name("home");
+Route::get('/products/service',[ProductController::class,'showProducts'])->name("products");
+Route::get('/contact',[ProductController::class,'contact'])->name("contact");
+Route::get('/about',[ProductController::class,'about'])->name("about");
+Route::get('/teams',[ProductController::class,'teams'])->name("teams");
+
+
